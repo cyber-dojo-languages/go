56319a857681ab8e6a1bfb994cbeff8e7591ead7
Jon Jagger <jon@jaggersoft.com>
2020-02-02 10:48:48 +0000

    [ci skip] Move version-number into check_version.sh

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 # Go
 
-[Version=1.13.4](https://github.com/cyber-dojo-languages/go/blob/master/check_version.sh)
-
 ![cyber-dojo.org home page](https://github.com/cyber-dojo/cyber-dojo/blob/master/shared/home_page_snapshot.png)
--- check_version.sh
+++ check_version.sh
@@ -6,7 +6,7 @@ readonly JSON=`cat docker/image_name.json`
 readonly IMAGE_NAME="${BASH_REMATCH[1]}"
 
 readonly MY_DIR="$( cd "$( dirname "${0}" )" && pwd )"
-readonly EXPECTED=$(cat ${MY_DIR}/README.md | grep Version | cut -d'=' -f2 | cut -d']' -f1)
+readonly EXPECTED=1.13.4
 readonly ACTUAL=$(docker run --rm -it ${IMAGE_NAME} sh -c 'go version')
 
 if echo "${ACTUAL}" | grep -q "${EXPECTED}"; then
@@ -14,5 +14,5 @@ if echo "${ACTUAL}" | grep -q "${EXPECTED}"; then
 else
   echo "VERSION EXPECTED: ${EXPECTED}"
   echo "VERSION   ACTUAL: ${ACTUAL}"
-  exit 1
+  exit 42
 fi
