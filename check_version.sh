51d8c7439eff7091e97a7616345e88674f136a07
Jon Jagger <jon@jaggersoft.com>
2020-03-01 06:21:45 +0000

    Go now at 1.14

--- check_version.sh
+++ check_version.sh
@@ -6,7 +6,7 @@ readonly JSON=`cat docker/image_name.json`
 readonly IMAGE_NAME="${BASH_REMATCH[1]}"
 
 readonly MY_DIR="$( cd "$( dirname "${0}" )" && pwd )"
-readonly EXPECTED=1.13.8
+readonly EXPECTED=1.14
 readonly ACTUAL=$(docker run --rm -it ${IMAGE_NAME} sh -c 'go version')
 
 if echo "${ACTUAL}" | grep -q "${EXPECTED}"; then
