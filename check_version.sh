cb2217ba4e2e66e1f28492397ba273d4538ead87
Faye <faye@kosli.com>
2025-01-20 10:34:16 +0100

    Upgrade to Go 1.23.5

--- check_version.sh
+++ check_version.sh
@@ -6,7 +6,7 @@ readonly JSON=`cat docker/image_name.json`
 readonly IMAGE_NAME="${BASH_REMATCH[1]}"
 
 readonly MY_DIR="$( cd "$( dirname "${0}" )" && pwd )"
-readonly EXPECTED=1.23.4
+readonly EXPECTED=1.23.5
 readonly ACTUAL=$(docker run --rm -i ${IMAGE_NAME} sh -c 'go version')
 
 if echo "${ACTUAL}" | grep -q "${EXPECTED}"; then
